1d6cc2113448ea4e5b38abf568fa7cb5fbb565b7
Ajit Singh <a2s@outlook.com>
2020-08-20 22:46:57 +0530

    Update BookService.java

--- src/main/java/com/genpact/librarymanagement/service/BookService.java
+++ src/main/java/com/genpact/librarymanagement/service/BookService.java
@@ -1,5 +1,6 @@
 package com.genpact.librarymanagement.service;
 
+import com.genpact.librarymanagement.exception.ResourceNotFoundException;
 import com.genpact.librarymanagement.model.Book;
 import com.genpact.librarymanagement.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,39 @@ public class BookService {
     public Optional<Book> findById(Long bid) {
         return bookRepository.findById(bid);
     }
+    
+    
+    public Book createBook(Book book) {
+		return bookRepository.save(book);
+	}
+    
+    
+    public Book updateBook(Book book) throws ResourceNotFoundException {
+		Optional<Book> BookDb = this.bookRepository.findById(book.getId());
+		
+		if(BookDb.isPresent()) {
+			Book bookUpdate = BookDb.get();
+			bookUpdate.setId(book.getId());
+			bookUpdate.setTitle(book.getTitle());
+			bookUpdate.setISBN(book.getISBN());
+			bookUpdate.setAuthor(book.getAuthor());
+			bookRepository.save(bookUpdate);
+			return bookUpdate;
+		}else {
+			throw new ResourceNotFoundException("Record not found with id : " + book.getId());
+		}		
+	}
+    
+    
+    public void deleteBook(long bookId) throws ResourceNotFoundException {
+		Optional<Book> bookDb = this.bookRepository.findById(bookId);
+		
+		if(bookDb.isPresent()) {
+			this.bookRepository.delete(bookDb.get());
+		}else {
+			throw new ResourceNotFoundException("Record not found with id : " + bookId);
+		}
+		
+	}
+
 }
