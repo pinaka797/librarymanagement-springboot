e8c81202fcce87c00dc57c909956d4a1c0368237
Ajit Singh <a2s@outlook.com>
2020-08-20 22:47:35 +0530

    Update Book.java

--- src/main/java/com/genpact/librarymanagement/model/Book.java
+++ src/main/java/com/genpact/librarymanagement/model/Book.java
@@ -7,13 +7,48 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Data
+//@Data
 public class Book {
     @Id
     @GeneratedValue
-    private long bid;
+    private long id;
     private String title;
     private String author;
     private long ISBN;
+    
+    
+    public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public long getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(long iSBN) {
+		ISBN = iSBN;
+	}
+
+	public long getId() {
+		return id;
+	}
+    
+    
 
 }
