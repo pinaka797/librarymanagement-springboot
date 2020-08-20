072a4585290478c46feece09eda71b1964cadc08
Ajit Singh <a2s@outlook.com>
2020-08-20 22:48:02 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/genpact/librarymanagement/exception/ResourceNotFoundException.java
+++ src/main/java/com/genpact/librarymanagement/exception/ResourceNotFoundException.java
@@ -11,4 +11,8 @@ public class ResourceNotFoundException extends Exception{
     public ResourceNotFoundException(String message){
         super(message);
     }
+    
+    public ResourceNotFoundException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
 }
